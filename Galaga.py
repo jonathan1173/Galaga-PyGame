945a8fc6bd89ad09123cdca874369c057f3acc53
jonathan106e@gmail.com <Jonathan106e@gmail.com>
2025-03-27 18:41:02 -0600

    destruciones de naves

--- Galaga.py
+++ Galaga.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import time
+import random
 
 pygame.init()
 
@@ -21,11 +22,18 @@ class Naves:
         self.velocidad = velocidad
         self.vida = vida
         self.daño = daño
-        self.jugador = jugador 
+        self.jugador = jugador
+        self.ultimo_disparo = 0
 
     def muerte(self):
+        self.vida -= self.daño
         if self.vida <= 0:
-            print("La nave ha sido destruida")
+            if self.jugador:
+                print("Has sido destruido")
+            else:
+                print("La nave enemiga ha sido destruida")
+            return True
+        return False
 
     def dibujar(self, pantalla):
         color = color_jugador if self.jugador else color_enemigo
@@ -35,33 +43,36 @@ class Naves:
     def disparar(self, frecuencia_disparo):
         tiempo_actual = time.time()
         if tiempo_actual - self.ultimo_disparo >= frecuencia_disparo:
-            balas.append(Balas(self.x  + 18, self.y, 5, 1, jugador=True))
-            self.ultimo_disparo = tiempo_actual  
-
+            if self.jugador:
+                balas.append(Balas(self.x + 18, self.y, 5, 1, jugador=True))  
+            else:
+                balas.append(Balas(self.x + 18, self.y + 40, 5, 1, jugador=False))  
+            self.ultimo_disparo = tiempo_actual
 
 class Balas:
-    def __init__(self, x, y, velocidad, daño, jugador):
+    def __init__(self, x, y, velocidad, daño, jugador=False):
         self.x = x
         self.y = y
         self.velocidad = velocidad
         self.daño = daño
-        self.jugador = jugador
+        self.jugador = jugador  # Se corrige la asignación correcta
 
     def mover(self, pantalla):
-        self.y -= self.velocidad
-        pygame.draw.line(pantalla, (255, 255, 255), (self.x , self.y), (self.x , self.y - 10), 2)
+        if self.jugador:
+            self.y -= self.velocidad  
+        else:
+            self.y += self.velocidad 
+        pygame.draw.line(pantalla, (255, 255, 255), (self.x, self.y), (self.x, self.y - 10), 2)
 
 class Jugador(Naves):
     def __init__(self, x, y, velocidad, vida, daño):
         super().__init__(x, y, velocidad, vida, daño, jugador=True)
-        self.ultimo_disparo = 0  
 
     def mover(self, teclas):
         if teclas[pygame.K_LEFT] and self.x > 0:
             self.x -= self.velocidad
         if teclas[pygame.K_RIGHT] and self.x < ANCHO_PANTALLA - 40:
             self.x += self.velocidad
-                
 
 class Enemigo(Naves):
     def __init__(self, x, y, velocidad, vida, daño):
@@ -72,10 +83,10 @@ def formacion_enemigos_piramide():
     filas = 5
     for i in range(filas):
         for j in range(filas - i):
-            pos_x = j * (30 + 20) 
+            pos_x = j * (30 + 20)
             pos_y = 50 + i * (30 + 20)
             margen = ANCHO_PANTALLA // 2 - (filas - i) * (30 + 20) // 2
-            enemigos.append(Enemigo(pos_x + margen, pos_y, 1, 1, 1))
+            enemigos.append(Enemigo(pos_x + margen, pos_y, 1, 2, 1))
     return enemigos
 
 jugador = Jugador(230, 400, 5, 3, 1)
@@ -83,29 +94,41 @@ enemigos = formacion_enemigos_piramide()
 
 ejecutando = True
 while ejecutando:
-    pantalla.fill((0, 0, 0)) 
-
+    pantalla.fill((0, 0, 0))
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutando = False
 
     teclas = pygame.key.get_pressed()
     jugador.mover(teclas)
-
-    if teclas[pygame.K_SPACE]:  
-        jugador.disparar(frecuencia_disparo=1)
-
+    if teclas[pygame.K_SPACE]:
+        jugador.disparar(frecuencia_disparo=0.5)  
     jugador.dibujar(pantalla)
-    for enemigo in enemigos:
-        enemigo.dibujar(pantalla)
     
+    for enemigo in enemigos[:]:
+        enemigo.dibujar(pantalla)
+        # enemigo.disparar(frecuencia_disparo=2)
+        
     for bala in balas[:]:
         bala.mover(pantalla)
-        if bala.y < 0: 
+        if bala.y < 0 or bala.y > ALTO_PANTALLA:  
             balas.remove(bala)
+        else:
+            if bala.jugador:  # Se corrige la referencia incorrecta
+                for enemigo in enemigos[:]:
+                    if enemigo.x < bala.x < enemigo.x + 40 and enemigo.y < bala.y < enemigo.y + 40:
+                        if enemigo.muerte():
+                            enemigos.remove(enemigo)
+                        try:
+                            balas.remove(bala)
+                        except ValueError:
+                            pass
+            else:
+                # Placeholder: implementar colisión de bala enemiga con el jugador
+                pass
 
-    pygame.display.flip()  
-    pygame.time.delay(30) 
+    pygame.display.flip()
+    pygame.time.delay(30)
 
 pygame.quit()
 sys.exit()
